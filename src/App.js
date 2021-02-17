5cfe2ba31070d0afd4329e8405f1b3150f565c9b
Helene <helebo@stud.ntnu.no>
2021-02-17 11:12:02 +0100

    Finished react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 
 function App() {
+
+    //Authentication
+  const APP_ID = '';
+  const APP_KEY = '';
+
+  // States
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('beef');
+
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  // Get data
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyApp> 
+      <SearchForm onSubmit={getSearch}>
+        <SearchBar type="text" value={search} onChange={updateSearch} />
+        <SearchButton>
+          Search
+        </SearchButton>
+      </SearchForm>
+      {recipes.map(recipe => (
+        <Recipe 
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))
+      };
+    </MyApp>
   );
 }
 
 export default App;
+
+const MyApp = styled.div`
+  min-height: 100vh;
+  background-image: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);
+`;
+
+const H1 = styled.h1`
+`;
+
+const SearchForm = styled.form`
+  min-height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SearchBar = styled.input`
+  width: 50%;
+  border: none;
+  padding: 10px;
+  background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+`;
+
+const SearchButton = styled.button`
+`;
---
+++ src/Recipe.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+// Recipe component
+const Recipe = ({ title, calories, image, ingredients }) => {
+    return (
+        <RecipeCard>
+            <H2>{title}</H2>
+            <Ol>
+                {ingredients.map(ingredient =>(
+                    <Li>{ingredient.text}</Li>
+                ))} 
+            </Ol>
+            <P>{calories}</P>
+            <RecipeImage src={image} alt=""></RecipeImage>
+        </RecipeCard>
+    )
+};
+
+export default Recipe;
+
+// styled components
+const RecipeCard = styled.div`
+`;
+
+const H2 = styled.h2`
+`;
+
+const P = styled.p`
+`;
+
+const RecipeImage = styled.img`
+`;
+
+const Ol = styled.ol`
+`;
+
+const Li = styled.li`
+`;
